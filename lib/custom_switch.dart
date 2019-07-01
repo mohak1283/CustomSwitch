206a3c8c339b16dcc7cd9a3c4a66bf687761645e
mohakgupta <mohak1283@gmail.com>
2019-07-01 16:14:45 +0530

    Some changes done

--- lib/custom_switch.dart
+++ lib/custom_switch.dart
@@ -25,10 +25,10 @@ class _CustomSwitchState extends State<CustomSwitch>
     _animationController =
         AnimationController(vsync: this, duration: Duration(milliseconds: 60));
     _circleAnimation = AlignmentTween(
-        begin: widget.value ? Alignment.centerRight : Alignment.centerLeft,
-        end: widget.value ? Alignment.centerLeft : Alignment.centerRight)
+            begin: widget.value ? Alignment.centerRight : Alignment.centerLeft,
+            end: widget.value ? Alignment.centerLeft : Alignment.centerRight)
         .animate(CurvedAnimation(
-        parent: _animationController, curve: Curves.linear));
+            parent: _animationController, curve: Curves.linear));
   }
 
   @override
@@ -36,45 +36,45 @@ class _CustomSwitchState extends State<CustomSwitch>
     return AnimatedBuilder(
       animation: _animationController,
       builder: (context, child) {
-        return Container(
-          width: 70.0,
-          height: 35.0,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: _circleAnimation.value == Alignment.centerLeft
-                  ? Colors.grey
-                  : widget.activeColor),
-          child: Padding(
-            padding: const EdgeInsets.only(
-                top: 4.0, bottom: 4.0, right: 4.0, left: 4.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                _circleAnimation.value == Alignment.centerRight
-
-                    ? Padding(
-                  padding: const EdgeInsets.only(left: 4.0, right: 4.0),
-                  child: Text(
-                    'On',
-                    style: TextStyle(
-                        color: Colors.white70,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 16.0),
-                  ),
-                )
-                    : Container(),
-                Align(
-                  alignment: _circleAnimation.value,
-                  child: GestureDetector(
-                    onTap: () {
-                      if (_animationController.isCompleted) {
-                        _animationController.reverse();
-                      } else {
-                        _animationController.forward();
-                      }
-                      widget.value == false ? widget.onChanged(true) : widget
-                          .onChanged(false);
-                    },
+        return GestureDetector(
+          onTap: () {
+            if (_animationController.isCompleted) {
+              _animationController.reverse();
+            } else {
+              _animationController.forward();
+            }
+            widget.value == false
+                ? widget.onChanged(true)
+                : widget.onChanged(false);
+          },
+          child: Container(
+            width: 70.0,
+            height: 35.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: _circleAnimation.value == Alignment.centerLeft
+                    ? Colors.grey
+                    : widget.activeColor),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 4.0, bottom: 4.0, right: 4.0, left: 4.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  _circleAnimation.value == Alignment.centerRight
+                      ? Padding(
+                          padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                          child: Text(
+                            'On',
+                            style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 16.0),
+                          ),
+                        )
+                      : Container(),
+                  Align(
+                    alignment: _circleAnimation.value,
                     child: Container(
                       width: 25.0,
                       height: 25.0,
@@ -82,21 +82,20 @@ class _CustomSwitchState extends State<CustomSwitch>
                           shape: BoxShape.circle, color: Colors.white),
                     ),
                   ),
-                ),
-
-                _circleAnimation.value == Alignment.centerLeft
-                    ? Padding(
-                  padding: const EdgeInsets.only(left: 4.0, right: 5.0),
-                  child: Text(
-                    'Off',
-                    style: TextStyle(
-                        color: Colors.white70,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 16.0),
-                  ),
-                )
-                    : Container(),
-              ],
+                  _circleAnimation.value == Alignment.centerLeft
+                      ? Padding(
+                          padding: const EdgeInsets.only(left: 4.0, right: 5.0),
+                          child: Text(
+                            'Off',
+                            style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 16.0),
+                          ),
+                        )
+                      : Container(),
+                ],
+              ),
             ),
           ),
         );
