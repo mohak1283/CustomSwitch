b0d9775aaa75b13d5d77abe4c8fe3129d1e8d03f
georgiani <georgiani@ github.com>
2020-05-15 16:41:44 +0200

    Fixed the constructor

--- lib/custom_switch.dart
+++ lib/custom_switch.dart
@@ -6,22 +6,22 @@ class CustomSwitch extends StatefulWidget {
   final bool value;
   final ValueChanged<bool> onChanged;
   final Color activeColor;
-  final Color inactiveColor = Colors.grey;
-  final String activeText = 'On';
-  final String inactiveText = 'Off';
-  final Color activeTextColor = Colors.white70;
-  final Color inactiveTextColor = Colors.white70;
+  final Color inactiveColor;
+  final String activeText;
+  final String inactiveText;
+  final Color activeTextColor;
+  final Color inactiveTextColor;
 
-  const CustomSwitch({
-    Key key, 
-    this.value, 
-    this.onChanged, 
-    this.activeColor, 
-    this.inactiveColor, 
-    this.activeText,
-    this.inactiveText,
-    this.activeTextColor,
-    this.inactiveTextColor})
+  const CustomSwitch(
+      {Key key,
+      this.value,
+      this.onChanged,
+      this.activeColor,
+      this.inactiveColor = Colors.grey,
+      this.activeText = 'On',
+      this.inactiveText = 'Off',
+      this.activeTextColor = Colors.white70,
+      this.inactiveTextColor = Colors.white70})
       : super(key: key);
 
   @override
@@ -36,13 +36,18 @@ class _CustomSwitchState extends State<CustomSwitch>
   @override
   void initState() {
     super.initState();
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 60));
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 40),
+    );
+
     _circleAnimation = AlignmentTween(
-            begin: widget.value ? Alignment.centerRight : Alignment.centerLeft,
-            end: widget.value ? Alignment.centerLeft : Alignment.centerRight)
-        .animate(CurvedAnimation(
-            parent: _animationController, curve: Curves.linear));
+      begin: widget.value ? Alignment.centerRight : Alignment.centerLeft,
+      end: widget.value ? Alignment.centerLeft : Alignment.centerRight,
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.linear),
+    );
   }
 
   @override
@@ -52,14 +57,17 @@ class _CustomSwitchState extends State<CustomSwitch>
       builder: (context, child) {
         return GestureDetector(
           onTap: () {
-            if (_animationController.isCompleted) {
-              _animationController.reverse();
-            } else {
-              _animationController.forward();
+            if (!_animationController.isAnimating) {
+              if (_animationController.isCompleted) {
+                _animationController.reverse();
+              } else {
+                _animationController.forward();
+              }
+              
+              widget.value == false
+                  ? widget.onChanged(true)
+                  : widget.onChanged(false);
             }
-            widget.value == false
-                ? widget.onChanged(true)
-                : widget.onChanged(false);
           },
           child: Container(
             width: 70.0,
